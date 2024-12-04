d4a77a3cfc0dcddfcadf5bb7f2674dcf9711f8bc
Valentin Peron <valentin.peron@epitech.eu>
2024-12-04 10:58:35 +0100

    feat(chatbot): enhance query processing by generating improved queries for retrieval

--- backend/routers/chatbot.py
+++ backend/routers/chatbot.py
@@ -6,10 +6,15 @@ router = APIRouter()
 
 @router.post("/chatbot/")
 async def chatbot(query: str):
-    # Run the retrieval chain and capture detailed results
-    answer1_with_knowledge = retrieval_chain.invoke({"input": query, "top_k": 1}) #FIXME: Update the top_k value
+    # Run the retrieval chain and capture detailed result
+    # generate the new query based on the actual query to be used in the retrieval
+
+    prompt = "Update the actual question to make it more proficient in the retrieval, i want you to upgrade the quality of the question and to only write the question as the response because i'm going to call a llm directly after it"
+    new_query = llm.invoke(query + "\n" + prompt).content
+    answer1_with_knowledge = retrieval_chain.invoke({"input": new_query, "top_k": 1}) #FIXME: Update the top_k value
     return {
         "query": query,
+        "better_query": new_query,
         "response": answer1_with_knowledge['answer'],
         "context": answer1_with_knowledge['context']
     }
