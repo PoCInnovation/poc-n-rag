e9816f812dc6bb9562b9248e4b85d7b1cc7f051b
Valentin Peron <valentin.peron@epitech.eu>
2025-03-12 11:43:54 +0100

    chore: update .gitignore to exclude additional file types and remove obsolete log and test files

--- test.py
+++
@@ -1,6 +0,0 @@
-import httpx
-
-url = 'http://127.0.0.1:8083/streaming/ask?query=How are you, write in 10 sentences'
-with httpx.stream('GET', url) as r:
-    for chunk in r.iter_raw():  # or, for line in r.iter_lines():
-        print(chunk)
